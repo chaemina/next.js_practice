b35a28c57ca8ccc530cf1ab77756b00ad404b4fb
chaemina <chaemina82@naver.com>
2023-12-27 17:49:20 +0900

    feat : another root 파일들에 적용되는 레이아웃 정의
    next.js의 명명 규칙을 사용하여, 또 다른 루트 레이아웃을 생성함

---
+++ src/app/(another-root)/layout.jsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <h1>다른 루트 레이아웃</h1>
+        {children}
+      </body>
+    </html>
+  );
+}
