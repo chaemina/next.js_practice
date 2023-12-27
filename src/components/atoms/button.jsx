39f4178a971e090217e08e493608f62b81bee20b
chaemina <chaemina82@naver.com>
2023-12-27 17:41:40 +0900

    fix : styled-components로 버튼 재구현

--- src/components/atoms/button.jsx
+++ src/components/atoms/button.jsx
@@ -1,10 +1,18 @@
+"use client";
+
 import React from "react";
+import styled from "styled-components";
+
+const BlueButton = styled.button`
+  background-color: white;
+  border: solid 2px blue;
+`;
 
 const Btn = React.forwardRef(({ onClick, href }, ref) => {
   return (
-    <button onClick={onClick} ref={ref}>
+    <BlueButton onClick={onClick} ref={ref}>
       <a href={href}>page 3</a>
-    </button>
+    </BlueButton>
   );
 });
 
